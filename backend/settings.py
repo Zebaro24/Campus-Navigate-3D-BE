04f0c40f12b5ed5468b43a4949ed3a525fb10b83
Zebaro <denok100100@gmail.com>
2025-06-02 01:26:32 +0200

    Швидке виправлення виводу зображень

--- backend/settings.py
+++ backend/settings.py
@@ -142,7 +142,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+IMAGES_URL = '/images/'
+IMAGES_ROOT = BASE_DIR / 'images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
